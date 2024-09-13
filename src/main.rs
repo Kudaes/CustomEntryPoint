563972311d7d5bc2a14ab92a3080fcee98cbec91
Kudaes <kurosh_2907@hotmail.com>
2024-09-13 09:04:30 +0200

    Remove call to loadlibrary
    This prevents the execution of TLS callbacks and entry point

--- src/main.rs
+++ src/main.rs
@@ -20,20 +20,26 @@ fn main() {
             Err(x) => {println!("{}",x);print!("{}","[x] Invalid arguments. Use -h for detailed help."); return; }
         };
 
-        if matches.opt_present("h") || !matches.opt_present("i") ||!matches.opt_present("o") || !matches.opt_present("f"){
+        if matches.opt_present("h") || !matches.opt_present("i") || !matches.opt_present("o") || (!matches.opt_present("f") && !matches.opt_present("e")){
             print_usage(&program, opts);
             return;
         }
 
         let input = matches.opt_str("i").unwrap();
         let output = matches.opt_str("o").unwrap();
-        let function = matches.opt_str("f").unwrap();
+        let mut function = String::new();
+        if matches.opt_present("f") 
+        {
+            function = matches.opt_str("f").unwrap();
+
+        }
 
         let file_content = fs::read(&input).expect("[x] Error opening the specified dll.");
         let pe_ptr = file_content.as_ptr() as *mut u8;
-        let mapped_pe = dinvoke_rs::dinvoke::load_library_a(&input);
+        let mapping_result = dinvoke_rs::manualmap::read_and_map_module(&input, false, false).unwrap();
+        let mapped_pe = mapping_result.1;
         let function_addr = dinvoke_rs::dinvoke::get_function_address(mapped_pe, &function);
-        if function_addr == 0
+        if function_addr == 0 && function != String::new()
         {
             println!("[x] The dll does not export any function with name {}", function);
             return;
@@ -45,15 +51,19 @@ fn main() {
         {
             let charact = (pe_ptr as usize + e_lfanew as usize + 0x4 + 0x13) as *mut u8; 
             *charact = 0;
-            println!("[+] Dll's characteristics have been set to 0. The extension can be switched to \".exe\".");
+            println!("[+] Dll's characteristics have been set to 0. The file can now be run directly.");
         } 
 
-        println!("[-] Patching entry point at RVA 0x{:x}...", e_lfanew + 0x18 + 16);
-        let entry = (pe_ptr as usize +  e_lfanew + 0x18 + 16) as *mut u32;
-        *entry = (function_addr-mapped_pe) as u32;
+        if function != String::new() 
+        {
+            println!("[-] Patching entry point at RVA 0x{:x}...", e_lfanew + 0x18 + 16);
+            let entry = (pe_ptr as usize +  e_lfanew + 0x18 + 16) as *mut u32;
+            *entry = (function_addr - mapped_pe) as u32;
+        }
+        
         let mut file = std::fs::File::create(&output).unwrap();
         let _r = file.write(file_content.as_slice()).unwrap();
-        println!("[+] Done! New dll written to {}.", output);
+        println!("[+] Done! New file written to {}.", output);
     
     }
    
